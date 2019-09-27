ac62d7f9a126893a3a037d6e9250d8292f0b66b1
SianLouisa <sianlouisa@hotmail.com>
2019-09-27 10:48:57 +0200

    update addition function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function addition(n1, n2) {
-	if (typeof n1 !== Number && typeof n2 !== Number)
+	if (typeof n1 !== number && typeof n2 !== number)
 		throw new TypeError("Numbers required!");
 	return n1 + n2;
 };
